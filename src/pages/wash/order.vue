68684f5948e151e4bf78e042c40492924a08b966
zch233 <529743595@qq.com>
2020-02-17 13:45:37 +0800

    Modify navig => redirectTo

--- src/pages/wash/order.vue
+++ src/pages/wash/order.vue
@@ -187,7 +187,7 @@
         //   return
         // }
 				// uni.hideLoading();
-				uni.navigateTo({ url: '/pages/wash/paySuccess' })
+				uni.redirectTo({ url: '/pages/wash/paySuccess' })
 			}
     }
   }
